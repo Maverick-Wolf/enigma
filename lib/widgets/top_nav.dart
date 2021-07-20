4215590784a508060d4710e8f7c9b096736e69bb
rachit <rachau76@gmail.com>
2021-07-20 17:36:21 +0530

    added hover color on buttons in navbar

--- lib/widgets/top_nav.dart
+++ lib/widgets/top_nav.dart
@@ -1,6 +1,7 @@
 import 'package:enigma/widgets/responsiveness.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hovering/hovering.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
@@ -24,40 +25,54 @@ AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
                 Spacer(),
                 Spacer(),
                 Spacer(),
-                InkWell(
-                    child: Container(
-                        child: Text(
-                  "HOME",
-                  style: TextStyle(fontSize: 15.0),
-                ))),
                 Spacer(),
-                InkWell(
-                    child: Container(
-                        child: Text(
-                  "ABOUT",
-                  style: TextStyle(fontSize: 15.0),
-                ))),
+                HoverButton(
+                  onpressed: () {},
+                  hoverColor: Colors.black,
+                  child: Text(
+                    "HOME",
+                    style: TextStyle(fontSize: 15.0, color: Colors.white),
+                  ),
+                ),
                 Spacer(),
-                InkWell(
-                    child: Container(
-                        child: Text(
-                  "SKILLS",
-                  style: TextStyle(fontSize: 15.0),
-                ))),
+                HoverButton(
+                  hoverShape: RoundedRectangleBorder(),
+                  onpressed: () {},
+                  hoverColor: Colors.black,
+                  child: Text(
+                    "ABOUT",
+                    style: TextStyle(fontSize: 15.0, color: Colors.white),
+                  ),
+                ),
                 Spacer(),
-                InkWell(
-                    child: Container(
-                        child: Text(
-                  "PROJECTS",
-                  style: TextStyle(fontSize: 15.0),
-                ))),
+                HoverButton(
+                  onpressed: () {},
+                  hoverColor: Colors.black,
+                  child: Text(
+                    "SKILLS",
+                    style: TextStyle(fontSize: 15.0, color: Colors.white),
+                  ),
+                ),
                 Spacer(),
-                InkWell(
-                    child: Container(
-                        child: Text(
-                  "CONTACT",
-                  style: TextStyle(fontSize: 15.0),
-                ))),
+                HoverButton(
+                  onpressed: () {},
+                  hoverColor: Colors.black,
+                  minWidth: 100.0,
+                  child: Text(
+                    "PROJECTS",
+                    style: TextStyle(fontSize: 15.0, color: Colors.white),
+                  ),
+                ),
+                Spacer(),
+                HoverButton(
+                  onpressed: () {},
+                  hoverColor: Colors.black,
+                  minWidth: 100.0,
+                  child: Text(
+                    "CONTACT",
+                    style: TextStyle(fontSize: 15.0, color: Colors.white),
+                  ),
+                ),
                 Spacer(),
               ],
             )
