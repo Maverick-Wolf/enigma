5c99efaa44d93df24b78c7544df79a46af377a78
rachit <rachau76@gmail.com>
2021-07-27 10:46:19 +0530

    added animation to the home page of desktop mode

--- lib/screens/home/desktop_home.dart
+++ lib/screens/home/desktop_home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:js' as js;
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class DesktopHome extends StatelessWidget {
   @override
@@ -14,140 +15,143 @@ class DesktopHome extends StatelessWidget {
             Spacer(
               flex: 2,
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Spacer(
-                  flex: 6,
-                ),
-                Text(
-                  "WELCOME TO MY PORTFOLIO!!",
-                  style: TextStyle(color: Colors.white, fontSize: 22.0),
-                ),
-                Spacer(),
-                Text(
-                  "Rachit",
-                  style: TextStyle(color: Colors.redAccent, fontSize: 40.0),
-                ),
-                Text(
-                  "Chaudhary",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 40.0),
-                ),
-                Spacer(),
-                Row(
+            AnimationLimiter(
+              child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "> ",
-                      style: TextStyle(
-                          color: Colors.redAccent,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 30.0),
-                    ),
-                    Container(
-                      width: 300.0,
-                      child: AnimatedTextKit(
-                        animatedTexts: [
-                          TypewriterAnimatedText("Flutter App Development",
-                              textStyle: TextStyle(
-                                  color: Colors.white, fontSize: 22.0),
-                              speed: Duration(milliseconds: 100)),
-                          TypewriterAnimatedText("Flutter Web Development",
-                              textStyle: TextStyle(
-                                  color: Colors.white, fontSize: 22.0),
-                              speed: Duration(milliseconds: 100)),
-                          TypewriterAnimatedText("Discord Bot Development",
-                              textStyle: TextStyle(
-                                  color: Colors.white, fontSize: 22.0),
-                              speed: Duration(milliseconds: 100)),
-                          TypewriterAnimatedText("Python Programming",
-                              textStyle: TextStyle(
-                                  color: Colors.white, fontSize: 22.0),
-                              speed: Duration(milliseconds: 100)),
-                        ],
-                        repeatForever: true,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: AnimationConfiguration.toStaggeredList(
+                    duration: Duration(milliseconds: 1300),
+                    childAnimationBuilder: (widget) => SlideAnimation(
+                      child: FadeInAnimation(
+                        child: widget,
                       ),
                     ),
-                  ],
-                ),
-                Spacer(
-                  flex: 2,
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                        icon: FaIcon(
-                          FontAwesomeIcons.github,
-                          size: 40.0,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          js.context.callMethod(
-                              'open', ['https://github.com/Maverick-Wolf']);
-                        }),
-                    SizedBox(
-                      width: 30.0,
-                    ),
-                    IconButton(
-                        icon: FaIcon(
-                          FontAwesomeIcons.linkedin,
-                          size: 40.0,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          js.context.callMethod('open', [
-                            'https://www.linkedin.com/in/rachit-chaudhary-8217b8204/'
-                          ]);
-                        }),
-                    SizedBox(
-                      width: 30.0,
-                    ),
-                    IconButton(
-                        icon: FaIcon(
-                          FontAwesomeIcons.reddit,
-                          color: Colors.white,
-                          size: 40.0,
-                        ),
-                        onPressed: () {
-                          js.context.callMethod('open',
-                              ['https://www.reddit.com/user/Branded-Devil']);
-                        }),
-                    SizedBox(
-                      width: 30.0,
-                    ),
-                    IconButton(
-                        icon: FaIcon(
-                          FontAwesomeIcons.instagram,
-                          size: 40.0,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          js.context.callMethod(
-                              'open', ['https://www.instagram.com/imrachitt/']);
-                        }),
-                    SizedBox(
-                      width: 30.0,
-                    ),
-                    IconButton(
-                        icon: FaIcon(
-                          FontAwesomeIcons.discord,
-                          size: 40.0,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          js.context.callMethod('open',
-                              ['https://top.gg/user/549820835230253060']);
-                        }),
-                  ],
-                ),
-                Spacer(
-                  flex: 6,
-                )
-              ],
+                    children: [
+                      Text(
+                        "WELCOME TO MY PORTFOLIO!!",
+                        style: TextStyle(color: Colors.white, fontSize: 22.0),
+                      ),
+                      Text(
+                        "Rachit",
+                        style:
+                            TextStyle(color: Colors.redAccent, fontSize: 40.0),
+                      ),
+                      Text(
+                        "Chaudhary",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 40.0),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "> ",
+                            style: TextStyle(
+                                color: Colors.redAccent,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 30.0),
+                          ),
+                          Container(
+                            width: 300.0,
+                            child: AnimatedTextKit(
+                              animatedTexts: [
+                                TypewriterAnimatedText(
+                                    "Flutter App Development",
+                                    textStyle: TextStyle(
+                                        color: Colors.white, fontSize: 22.0),
+                                    speed: Duration(milliseconds: 100)),
+                                TypewriterAnimatedText(
+                                    "Flutter Web Development",
+                                    textStyle: TextStyle(
+                                        color: Colors.white, fontSize: 22.0),
+                                    speed: Duration(milliseconds: 100)),
+                                TypewriterAnimatedText(
+                                    "Discord Bot Development",
+                                    textStyle: TextStyle(
+                                        color: Colors.white, fontSize: 22.0),
+                                    speed: Duration(milliseconds: 100)),
+                                TypewriterAnimatedText("Python Programming",
+                                    textStyle: TextStyle(
+                                        color: Colors.white, fontSize: 22.0),
+                                    speed: Duration(milliseconds: 100)),
+                              ],
+                              repeatForever: true,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                              icon: FaIcon(
+                                FontAwesomeIcons.github,
+                                size: 40.0,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                js.context.callMethod('open',
+                                    ['https://github.com/Maverick-Wolf']);
+                              }),
+                          SizedBox(
+                            width: 30.0,
+                          ),
+                          IconButton(
+                              icon: FaIcon(
+                                FontAwesomeIcons.linkedin,
+                                size: 40.0,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                js.context.callMethod('open', [
+                                  'https://www.linkedin.com/in/rachit-chaudhary-8217b8204/'
+                                ]);
+                              }),
+                          SizedBox(
+                            width: 30.0,
+                          ),
+                          IconButton(
+                              icon: FaIcon(
+                                FontAwesomeIcons.reddit,
+                                color: Colors.white,
+                                size: 40.0,
+                              ),
+                              onPressed: () {
+                                js.context.callMethod('open', [
+                                  'https://www.reddit.com/user/Branded-Devil'
+                                ]);
+                              }),
+                          SizedBox(
+                            width: 30.0,
+                          ),
+                          IconButton(
+                              icon: FaIcon(
+                                FontAwesomeIcons.instagram,
+                                size: 40.0,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                js.context.callMethod('open',
+                                    ['https://www.instagram.com/imrachitt/']);
+                              }),
+                          SizedBox(
+                            width: 30.0,
+                          ),
+                          IconButton(
+                              icon: FaIcon(
+                                FontAwesomeIcons.discord,
+                                size: 40.0,
+                                color: Colors.white,
+                              ),
+                              onPressed: () {
+                                js.context.callMethod('open',
+                                    ['https://top.gg/user/549820835230253060']);
+                              }),
+                        ],
+                      ),
+                    ],
+                  )),
             ),
             Spacer(
               flex: 3,
